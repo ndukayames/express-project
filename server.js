920d220672829ebe086ddb693cb166474b3f11c4
Obi James Onyemaechi <jamesobi@softprism.org>
2022-11-17 16:39:17 +0100

    implement posting and error handling

--- server.js
+++ server.js
@@ -32,11 +32,27 @@ SERVER.use((req, res, next) => {
 SERVER.get("/", (req, res) => {
   res.status(200).json("Hello World!");
 });
+SERVER.use(express.json());
 
 SERVER.get("/friends", (req, res, next) => {
   res.send(friends);
 });
 
+SERVER.post("/friends", (req, res) => {
+  // input validation
+  if (!req.body.name) {
+    return res.status(400).json({ error: "please enter friend's name" });
+  }
+  const newFriend = {
+    id: friends.length + 1,
+    name: req.body.name,
+  };
+
+  // add new friend to list
+  if (friends.push(newFriend)) {
+    res.status(201).json({ msg: "Friend added!" });
+  }
+});
 SERVER.get("/friends/:friendId", (req, res, next) => {
   let friendId = Number(req.params.friendId);
   let friend = friends.find((friend) => friend.id === friendId);
