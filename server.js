7783693e3b07097d0d379f5dbd15bf85dc003bed
Obi James Onyemaechi <jamesobi@softprism.org>
2022-11-17 17:45:20 +0100

    modularization and routing

--- server.js
+++ server.js
@@ -1,68 +1,26 @@
 const express = require("express");
 
-const SERVER = express();
+const friendsRoute = require("./routes/friends.route");
+
+const app = express();
 const PORT = 3000;
-
-// load friends
-
-const friends = [
-  {
-    id: 1,
-    name: "Obi James",
-  },
-  {
-    id: 2,
-    name: "Nduka Yames",
-  },
-  {
-    id: 3,
-    name: "Ndukayames",
-  },
-];
 
 // create a middleware that logs request
 
-SERVER.use((req, res, next) => {
+app.use((req, res, next) => {
   console.log(`sending ${req.method} request to ${req.url}`);
   next();
-  console.log(`finished ${req.method} request to ${req.url} }`);
+  console.log(`finished ${req.method} request to ${req.baseUrl}${req.url} }`);
 });
+// parsing json input
+app.use(express.json());
 
-// specifying routes, try 1;
-SERVER.get("/", (req, res) => {
+// specifying routes,
+app.use("/friends", friendsRoute);
+app.get("/", (req, res) => {
   res.status(200).json("Hello World!");
 });
-SERVER.use(express.json());
 
-SERVER.get("/friends", (req, res, next) => {
-  res.send(friends);
-});
-
-SERVER.post("/friends", (req, res) => {
-  // input validation
-  if (!req.body.name) {
-    return res.status(400).json({ error: "please enter friend's name" });
-  }
-  const newFriend = {
-    id: friends.length + 1,
-    name: req.body.name,
-  };
-
-  // add new friend to list
-  if (friends.push(newFriend)) {
-    res.status(201).json({ msg: "Friend added!" });
-  }
-});
-SERVER.get("/friends/:friendId", (req, res, next) => {
-  let friendId = Number(req.params.friendId);
-  let friend = friends.find((friend) => friend.id === friendId);
-
-  if (friend) {
-    res.status(200).send(friend);
-  } else {
-    res.status(404).send({ error: "Friend does not exist" });
-  }
-});
-SERVER.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Server is running");
 });
